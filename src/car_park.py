547cd2fc5b3cd331066412aeeda0810aad3b45b3
Artem Baranenko <artembaranenko1005@gmail.com>
2025-12-05 15:19:48 +0800

    Finished log feature

--- src/car_park.py
+++ src/car_park.py
@@ -1,19 +1,29 @@
+from pathlib import Path
+from datetime import datetime
+
 from .display import Display
 from .sensor import Sensor
 
 class CarPark:
-    def __init__(self, location, capacity, sensors = None, plates = None, displays = None):
+    def __init__(self, location, capacity, sensors = None, plates = None, displays = None, log_file=Path("log.txt") ):
         self.location = location or "Unknown"
         self.capacity = capacity or 0
         self.plates = plates or []
         self.displays = displays or []
         self.sensors = sensors or []
+        
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        self.log_file.touch(exist_ok=True)
 
     def __str__(self):
         return f"Car park at {self.location}, with {self.capacity} bays."
 
     def __repr__(self):
             return self.__str__()
+    
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
 
     def register(self, component):
         if not isinstance(component, (Sensor, Display)):
@@ -28,10 +38,12 @@ class CarPark:
     def add_car(self, plate):
         self.plates.append(plate)
         self.update_display()
+        self._log_car_activity(plate, "entered")
     
     def remove_car(self, plate):
         self.plates.remove(plate)
         self.update_display()
+        self._log_car_activity(plate, "exited")
 
     @property
     def available_bays(self):
